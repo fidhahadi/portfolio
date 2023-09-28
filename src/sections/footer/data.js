c363189a6874bbf70b3966ecaa33a035f703575d
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-28 21:56:59 +0400

    added linkedin

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -4,6 +4,7 @@
 import { AiFillGithub } from 'react-icons/ai'
 //import { AiFillTwitterSquare } from 'react-icons/ai'
 import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
 export const links = [
     { id: 1, link: '#', title: 'Home' },
     { id: 2, link: '#about', title: 'About' },
@@ -15,5 +16,6 @@ export const links = [
 
 export const socials = [
     { id: 1, link: 'https://twitter.com/fidha_rafeeque', icon: <AiOutlineTwitter /> },
-    { id: 2, link: 'https://github.com/fidhahadi', icon: <AiFillGithub /> }
+    { id: 2, link: 'https://github.com/fidhahadi', icon: <AiFillGithub /> },
+    { id: 5, link: 'https://www.linkedin.com/in/fidha-rafeeque-n-86802a105/', icon: <AiFillLinkedin /> }
 ]
--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -2,12 +2,14 @@
 import { AiOutlineTwitter } from 'react-icons/ai'
 //import { AiOutlineDribbble } from 'react-icons/ai'
 import { AiFillGithub } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
 
 const data = [
     // {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
     { id: 2, link: 'https://twitter.com/fidha_rafeeque', icon: <AiOutlineTwitter /> },
     //{ id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble /> },
-    { id: 4, link: 'https://github.com/fidhahadi', icon: <AiFillGithub /> }
+    { id: 4, link: 'https://github.com/fidhahadi', icon: <AiFillGithub /> },
+    { id: 5, link: 'https://www.linkedin.com/in/fidha-rafeeque-n-86802a105/', icon: <AiFillLinkedin /> }
 ]
 
 export default data
